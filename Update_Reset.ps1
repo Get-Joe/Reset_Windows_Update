2baa7d0504757f8a60191306c526bd7dd107bac2
Get-Joe <sithlord1977@gmail.com>
2018-10-30 02:06:16 -0400

    import

---
+++ Update_Reset.ps1
@@ -0,0 +1,37 @@
+﻿# Stop services
+
+net stop wuauserv
+
+net stop cryptsvc
+
+net stop bits
+
+net stop msiserver
+
+net stop appidsvc
+
+# Rename files
+
+ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+
+ren C:\Windows\system32\CatRoot2 CatRoot2.old
+
+# Start services
+
+net start wuauserv
+
+net start cryptsvc
+
+net start bits
+
+net start msiserver
+
+net start appidsvc
+
+# Run update executable
+
+.\wuauclt.exe /updatenow
+
+# Restart machine
+
+shutdown /r /f /t 0
